4a7b647b1a148476206a621fef1760bab048bdea
f1jamzi <f1jamzi@gmail.com>
2026-07-18 18:32:26 +0300

    Added base new Roboto fonts

--- Forkly_Mark_II/Forkly_Mark_II/ViewController.swift
+++ Forkly_Mark_II/Forkly_Mark_II/ViewController.swift
@@ -7,6 +7,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Test Label
+        let label = UILabel()
+        label.text = "Hello World"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
+        ])
+        
+       label.font = UIFont.systemFont(ofSize: 40, weight: .heavy)
+        label.font = .systemFont(ofSize: 40, weight: .heavy)
+        label.textColor = .white
+        
+        
         view.backgroundColor = .systemPink
 
     }
